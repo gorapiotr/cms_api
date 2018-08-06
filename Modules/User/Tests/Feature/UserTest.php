865b0ad2a02dc71bafdf53610c75605f402bb237
gorapiotr <pgora@sigma.ug.edu.pl>
2018-08-06 18:19:12 +0200

    Working at User module. Added tests.

--- Modules/User/Tests/Feature/UserTest.php
+++ Modules/User/Tests/Feature/UserTest.php
@@ -3,6 +3,7 @@
 namespace Modules\User\Tests;
 
 use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 
 class ReportSchemesUser
@@ -75,6 +76,89 @@ class UserTest extends TestCase
         $this->assertEquals(3, $response['id'], 'Id should be 3.');
         $this->assertEquals('mike', $response['name'], 'Name should be mike.');
         $this->assertEquals('mike@gmail.com', $response['email'], 'Email should be mike@gmail.com.');
+
+    }
+
+    public function testGetAuthUserData()
+    {
+        /* get authorization token */
+        $token = \JWTAuth::attempt([
+            'email' => 'admin@gmail.com',
+            'password' => 'adminadmin'
+        ]);
+
+
+        $response = $this->withoutMiddleware()
+            ->json(
+            'GET',
+            $this->testing_url,
+            [],
+            ['Authorization' => 'Bearer '.$token ]
+        )
+            ->assertStatus(200)
+            ->assertJsonStructure($this->schemes->show)
+            ->decodeResponseJson('data');
+
+        $this->assertEquals(1, $response['id'], 'Id should be 1.');
+        $this->assertEquals('admin', $response['name'], 'Name should be admin.');
+        $this->assertEquals('admin@gmail.com', $response['email'], 'Email should be admin@gmail.com.');
+
+        $this->withoutMiddleware()
+            ->json(
+                'POST',
+                'api/logout',
+                [],
+                ['Authorization' => 'Bearer '.$token ]
+            );
+
+        $this->assertEquals(null, Auth::id(), 'Id should be null.');
+
+    }
+
+    public function testUpdateUserData()
+    {
+        /* get authorization token */
+        $token = \JWTAuth::attempt([
+            'email' => 'admin@gmail.com',
+            'password' => 'adminadmin'
+        ]);
+
+        $response = $this->withoutMiddleware()
+            ->json(
+                'PUT',
+                $this->testing_url,
+                ['name' => 'john'],
+                ['Authorization' => 'Bearer '.$token ]
+            )->assertStatus(200)
+            ->assertJsonStructure($this->schemes->show)
+            ->decodeResponseJson('data');
+
+
+        $this->assertEquals('john', $response['name'], 'Name should be john.');
+
+        $response = $this->withoutMiddleware()
+            ->json(
+                'PUT',
+                $this->testing_url,
+                ['name' => 'admin'],
+                ['Authorization' => 'Bearer '.$token ]
+            )->assertStatus(200)
+            ->assertJsonStructure($this->schemes->show)
+            ->decodeResponseJson('data');
+
+        $this->assertEquals('admin', $response['name'], 'Name should be admin.');
+
+        $this->withoutMiddleware()
+            ->json(
+                'POST',
+                'api/logout',
+                [],
+                ['Authorization' => 'Bearer '.$token ]
+            );
+
+        $this->assertEquals(null, Auth::id(), 'Id should be null.');
+
+
 
     }
 
